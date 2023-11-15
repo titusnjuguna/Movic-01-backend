0af9c4f57678518f6ad808fbf267777bd7acbdb2
tito <titusnjuguna59@gmail.com>
2023-11-15 11:53:41 +0300

    Chore:first initial Commit

---
+++ Movie/src/main/java/com/example/Movie/MovieService.java
@@ -0,0 +1,20 @@
+package com.example.Movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> getSingleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+
+    }
+}
